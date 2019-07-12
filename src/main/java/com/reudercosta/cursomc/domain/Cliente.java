c01067abce94d568ca4e5a4d3426954e2a51e38b
Reuder Cerqueira <reudercerqueira@gmail.com>
2019-07-12 19:39:40 -0300

    Ajustes no endpoint /clientes/id

--- src/main/java/com/reudercosta/cursomc/domain/Cliente.java
+++ src/main/java/com/reudercosta/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.reudercosta.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -28,7 +29,8 @@ public class Cliente implements Serializable {
 	private String email;
 	private String cpfouCnpj;
 	private Integer tipo;
-
+	
+	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
