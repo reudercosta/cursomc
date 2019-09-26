2b2491cad452c065374ddccdacd0f75648855a2a
Reuder Cerqueira <reudercerqueira@gmail.com>
2019-09-26 18:18:10 -0300

    Criando profile de Desenvolvimento

--- src/main/java/com/reudercosta/cursomc/services/DBService.java
+++ src/main/java/com/reudercosta/cursomc/services/DBService.java
@@ -69,6 +69,7 @@ public class DBService {
 		Categoria cat5 = new Categoria(null, "Jardinagem");
 		Categoria cat6 = new Categoria(null, "Decoração");
 		Categoria cat7 = new Categoria(null, "Perfumaria");
+		Categoria cat8 = new Categoria(null, "Teste");
 
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -101,8 +102,9 @@ public class DBService {
 		p9.getCategorias().addAll(Arrays.asList(cat6));
 		p10.getCategorias().addAll(Arrays.asList(cat6));
 		p11.getCategorias().addAll(Arrays.asList(cat7));
+		
 
-		categoriaRepository.save(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
+		categoriaRepository.save(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7,cat8));
 		produtoRepository.save(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
 
 		Estado est2 = new Estado(null, "São Paulo");
