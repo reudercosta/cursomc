bfcd7373ae3b68fc0588614de7f077c426c26dba
Reuder Cerqueira <reudercerqueira@gmail.com>
2020-05-29 19:44:39 -0300

    ENDPOINT PARA BUSCAR CLIENTE POR EMAIL

--- src/main/java/com/reudercosta/cursomc/services/ClienteService.java
+++ src/main/java/com/reudercosta/cursomc/services/ClienteService.java
@@ -98,6 +98,19 @@ public class ClienteService {
 	public List<Cliente> findAll() {
 		return repo.findAll();
 	}
+	
+	public Cliente findByEmail(String email) {
+		UserSS user = UserService.authenticated();
+		if(user==null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		Cliente obj = repo.findByEmail(email);
+		if(obj==null) {
+			throw new ObjectNotFoundException("Objeto não Encontrado"+ user.getId()+ ", Tipo: "+Cliente.class.getName());
+			
+		}
+		return obj;
+	}
 
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
